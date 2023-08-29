85df5384c9c4024d780a920de4a176b8df82d13c
Beata Eriksson <be222gr@student.lnu.se>
2023-08-29 15:31:22 +0200

    Add some pokemon fetching

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
  * @version 1.0.0
  */
 
+
 const form = document.querySelector('#name-form')
 form.addEventListener('submit', handleSubmit)
 const result = document.querySelector('#result')
 
-function handleSubmit (event) {
+
+function handleSubmit(event) {
     event.preventDefault()
     const yourName = form.querySelector('#your-name')
-    
+
     while (result.childElementCount > 0) {
         result.removeChild(result.lastElementChild)
     }
@@ -24,10 +26,10 @@ function handleSubmit (event) {
     yourName.value = ''
 }
 
-function convertToNumber (string) {
+function convertToNumber(string) {
     const nameArray = string.toLowerCase().split('')
     const numberArray = []
-    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r' ,'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
     for (let i = 0; i < nameArray.length; i++) {
         numberArray.push(letters.indexOf(nameArray[i]) + 1)
     }
@@ -41,15 +43,42 @@ function convertToNumber (string) {
     sumNumbers(numberArray)
 }
 
-function sumNumbers (array) {
+function sumNumbers(array) {
     let sum = 0
     for (let i = 0; i < array.length; i++) {
         sum += array[i]
     }
     const newP = document.createElement('p')
-    newP.textContent = 'Your number is...'
-    const newH = document.createElement('h1')
-    newH.textContent = sum
+    newP.textContent = 'Your number is ' + sum
     result.appendChild(newP)
-    result.appendChild(newH)
+    fetchPokemon(sum)
+}
+
+async function fetchPokemon(number) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${number}/`)
+        if (!response.ok) {
+            const error = new Error(`Fetch error ${response.status}`)
+            error.message = `Fetch error ${response.status}`
+        }
+        const data = await response.json()
+        const nameOfPokemon = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+        const newP = document.createElement('p')
+        if (nameOfPokemon.includes('-m')) {
+            const editStr = nameOfPokemon.replace('-m', ' ♂')
+            newP.textContent = 'Your pokémon is ' +  editStr
+        } else if (nameOfPokemon.includes('-f')) {
+            const editStr = nameOfPokemon.replace('-f', ' ♀')
+            newP.textContent = 'Your pokémon is ' +  editStr
+        } else {
+            newP.textContent = 'Your pokémon is ' + nameOfPokemon
+        }
+        result.appendChild(newP)
+        const img = document.createElement('img')
+        img.src = data.sprites.front_default
+        result.appendChild(img)
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
