c17c40da06d210bb44bda9cc26f73cc2f3d61f85
Beata Eriksson <be222gr@student.lnu.se>
2023-08-29 14:59:06 +0200

    Edit css and finish calculations

--- index.js
+++ index.js
@@ -7,31 +7,37 @@
 
 const form = document.querySelector('#name-form')
 form.addEventListener('submit', handleSubmit)
+const result = document.querySelector('#result')
 
 function handleSubmit (event) {
     event.preventDefault()
     const yourName = form.querySelector('#your-name')
-
-    const result = document.querySelector('#result')
-    if (result.childElementCount > 0) {
-        result.removeChild(result.firstElementChild)
+    
+    while (result.childElementCount > 0) {
+        result.removeChild(result.lastElementChild)
     }
 
     const newP = document.createElement('p')
-    newP.textContent = yourName.value
+    newP.textContent = 'Hello, ' + yourName.value
     result.appendChild(newP)
     convertToNumber(yourName.value)
     yourName.value = ''
 }
 
 function convertToNumber (string) {
-    const nameArray = string.split('')
+    const nameArray = string.toLowerCase().split('')
     const numberArray = []
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r' ,'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
     for (let i = 0; i < nameArray.length; i++) {
         numberArray.push(letters.indexOf(nameArray[i]) + 1)
     }
-    console.log(numberArray)
+    // If you want to show the numbers:
+    /*
+    const str = numberArray.toString().replace(/,/g, ' + ')
+    const newP = document.createElement('p')
+    newP.textContent = str
+    result.appendChild(newP)
+    */
     sumNumbers(numberArray)
 }
 
@@ -40,5 +46,10 @@ function sumNumbers (array) {
     for (let i = 0; i < array.length; i++) {
         sum += array[i]
     }
-    console.log(sum)
+    const newP = document.createElement('p')
+    newP.textContent = 'Your number is...'
+    const newH = document.createElement('h1')
+    newH.textContent = sum
+    result.appendChild(newP)
+    result.appendChild(newH)
 }
