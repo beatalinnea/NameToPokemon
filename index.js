e5b2ed1da5fd199c1a9a1c48e28834e68c6534b8
Beata Eriksson <be222gr@student.lnu.se>
2023-08-29 12:19:46 +0200

    Add submitHandler

--- index.js
+++ index.js
@@ -5,6 +5,31 @@
  * @version 1.0.0
  */
 
-function hello () {
-    console.log('I am just testing my new computer')
+const form = document.querySelector('#name-form')
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit (event) {
+    event.preventDefault()
+    const inputElement = form.querySelector('#your-name')
+
+    const result = document.querySelector('#result')
+    if (result.childElementCount > 0) {
+        result.removeChild(result.firstElementChild)
+    }
+
+    const newP = document.createElement('p')
+    newP.textContent = inputElement.value
+    result.appendChild(newP)
+    calcNumber(inputElement.value)
+    inputElement.value = ''
+}
+
+function calcNumber (string) {
+    const nameArray = string.split('')
+    const numberArray = []
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r' ,'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+    for (let i = 0; i < nameArray.length; i++) {
+        numberArray.push(letters.indexOf(nameArray[i]) + 1)
+    }
+    console.log(numberArray)
 }
