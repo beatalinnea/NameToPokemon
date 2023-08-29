2f1565e0593e7891778865e97996ffd3f223d359
Beata Eriksson <be222gr@student.lnu.se>
2023-08-29 11:18:17 +0200

    Redo file structure

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Beata Eriksson <be222gr@student.lnu.se>
+ * @version 1.0.0
+ */
+
+function hello () {
+    console.log('I am just testing my new computer')
+}
